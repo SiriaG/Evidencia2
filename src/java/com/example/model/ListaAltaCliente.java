161afbc659842a9c483e250006cedbba1ba0c6ec
andywarhol <al02724314@tecmilenio.mx>
2020-03-28 19:05:12 -0700

    COmmit

---
+++ src/java/com/example/model/ListaAltaCliente.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.model;
+
+/**
+ *
+ * @author andi9
+ */
+public class ListaAltaCliente {
+    
+    private String nombre;
+    private String direccion;
+    private int cp;
+    private String ciudad;
+    private String estado;
+    private String pais;
+    private long telefono;
+    private String cumple;
+    private String correo;
+    private String contrasena;
+
+    public ListaAltaCliente(String nombre, String direccion, int cp, 
+            String ciudad, String estado, String pais, long telefono, 
+            String cumple, String correo, String contrasena) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.cp = cp;
+        this.ciudad = ciudad;
+        this.estado = estado;
+        this.pais = pais;
+        this.telefono = telefono;
+        this.cumple = cumple;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCumple() {
+        return cumple;
+    }
+
+    public void setCumple(String cumple) {
+        this.cumple = cumple;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contrasena;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contrasena = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaAltaCliente{" + "nombre=" + nombre + ", direccion=" 
+                + direccion + ", cp=" + cp + ", ciudad=" + ciudad + ", estado=" 
+                + estado + ", pais=" + pais + ", telefono=" + telefono 
+                + ", cumple=" + cumple + ", correo=" + correo + ", contrasena=" 
+                + contrasena + '}';
+    }
+    
+}
